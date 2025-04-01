9c885f456ccbca02c021fdbba97aad699ea0957c
Eulalie2003 <amigoeulalie@gmail.com>
2025-04-01 16:30:40 +0200

    trouver le min Eulalie

--- partie Eulalie.py
+++ partie Eulalie.py
@@ -9,10 +9,6 @@ athlete = pd.read_csv(
 # ne garde que les athlètes de 2016
 athlete_2016 = athlete[athlete["Year"] == 2016]
 
-# ne garde que les pays ayant remporté une médaille
-athletes_medailles = athlete_2016[athlete_2016["Medal"].isin([
-    "Bronze", "Gold", "Silver"])]
-
 # suprpime les doubles: quand l'épreuve est par équipe
 athletes_medailles = athlete_2016[["Medal", "NOC", "Event"]]
 athletes_medailles = athletes_medailles.drop_duplicates()
@@ -24,6 +20,6 @@ regroup = (
 
 # max des médailles remportées
 nb_max = max(regroup.sum())
-print(nb_max)
 
 # nombre minimum
+nb_min = min(regroup.sum())

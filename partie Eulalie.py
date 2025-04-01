8f12e2bf3e28a1f5e6a5b07a4403d6d1199d9a90
Eulalie2003 <amigoeulalie@gmail.com>
2025-04-01 17:06:33 +0200

    modification, question posées Eulalie

--- partie Eulalie.py
+++ partie Eulalie.py
@@ -4,7 +4,7 @@ import pandas as pd
 athlete = pd.read_csv(
     "donnees_jeux_olympiques/donnees_jeux_olympiques/athlete_events.csv"
 )
-# question 1
+# Question 1:
 # min et max de chaque pays
 # ne garde que les athlètes de 2016
 athlete_2016 = athlete[athlete["Year"] == 2016]
@@ -23,3 +23,8 @@ nb_max = max(regroup.sum())
 
 # nombre minimum
 nb_min = min(regroup.sum())
+
+# Question 2: Evolution des médailles hommes/femmes au fil des ans ?
+
+# Question 3 en python: athlete ayant le plus de médailles
+ 

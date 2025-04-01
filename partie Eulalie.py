f1bb94c0997f4899fd77dc2dac68727e798aa3af
Eulalie2003 <amigoeulalie@gmail.com>
2025-04-01 16:23:38 +0200

    correction de code Eulalie pour commpter le nombre de victoires aux JO 2016

--- partie Eulalie.py
+++ partie Eulalie.py
@@ -6,19 +6,24 @@ athlete = pd.read_csv(
 )
 # question 1
 # min et max de chaque pays
+# ne garde que les athlètes de 2016
 athlete_2016 = athlete[athlete["Year"] == 2016]
 
-
+# ne garde que les pays ayant remporté une médaille
 athletes_medailles = athlete_2016[athlete_2016["Medal"].isin([
     "Bronze", "Gold", "Silver"])]
 
+# suprpime les doubles: quand l'épreuve est par équipe
+athletes_medailles = athlete_2016[["Medal", "NOC", "Event"]]
+athletes_medailles = athletes_medailles.drop_duplicates()
+
+# compte le nombre de médaille par pays
 regroup = (
     athletes_medailles.groupby("NOC")["Medal"].value_counts().groupby("NOC")
     )
 
+# max des médailles remportées
 nb_max = max(regroup.sum())
-
+print(nb_max)
 
 # nombre minimum
-sans_medaille = athlete_2016[not(athlete_2016["Medal"].isin([
-    "Bronze", "Gold", "Silver"]))]

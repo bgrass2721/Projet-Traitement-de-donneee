98a9e3e507af25ad4beada05b524a7bb2bd081cb
Julie Malet <julie.malet@eleve.ensai.fr>
2025-04-11 16:34:07 +0200

    Début question Python pur

--- partie_Julie.py
+++ partie_Julie.py
@@ -23,6 +23,16 @@ with open(os.path.join(
     for line in file:
         res.append(line)
 
+country = []
+with open(os.path.join(
+    "donnees_jeux_olympiques", "donnees_jeux_olympiques", "noc_regions.csv"
+)) as fd:
+    file = csv.reader(fd)
+    for line in file:
+        country.append(line)
+
+'''
+
 # Le nombre de femmes parmi les participants par année.
 
 athlete_femme = athlete[athlete["Sex"] == "F"]
@@ -37,9 +47,9 @@ print("Nombre de femmes parmi les participants par année : ", nb_femme_an)
 
 
 proportion_femme_an = nb_femme_an.div(nb_athlete_an)
-print("Proportion de femmes par année :", proportion_femme_an)
+print("Proportion de femmes par année : ", proportion_femme_an)
 
-'''
+
 # La moyenne des médailles remportées par les athlètes du Royaume-Uni.
 
 athlete_UK = athlete[athlete["NOC"] == "GBR"]
@@ -50,6 +60,72 @@ medailles_UK_ete = athlete_UK_ete.groupby("Year")["Medal"].value_counts().groupb
 print("Médailles du Royaume-Uni : ", medailles_UK_ete)
 print("Moyenne des médailles du Royaume-Uni : ", medailles_UK_ete.mean())
 
+
+# La moyenne des médailles remportées par les athlètes de la Chine
+
+
+athlete_chine = athlete[athlete["NOC"] == "CHN"]
+print("Athlètes de la Chine : ", athlete_chine)
+athlete_chine_ete = athlete_chine[athlete_chine["Season"] == "Summer"]
+medailles_chine_ete = athlete_chine_ete.groupby("Year")["Medal"].value_counts().groupby("Year").sum()
+
+print("Médailles de la Chine : ", medailles_chine_ete)
+print("Moyenne des médailles de la Chine : ", medailles_chine_ete.mean())
+
+
+
+# La moyenne des médailles remportées par les athlètes de la Chine
+
+
+athlete_US = athlete[athlete["NOC"] == "USA"]
+print("Athlètes des Etats-Unis : ", athlete_US)
+athlete_US_ete = athlete_US[athlete_US["Season"] == "Summer"]
+medailles_US_ete = athlete_US_ete.groupby("Year")["Medal"].value_counts().groupby("Year").sum()
+
+print("Médailles des Etats-Unis : ", medailles_US_ete)
+print("Moyenne des médailles des Etats-Unis : ", medailles_US_ete.mean())
+'''
+
+
+
 # Combien de médailles les pays participants ont-ils gagné en moyenne par JO ?
 
+
+[ligne.pop(14) for ligne in res]
+[ligne.pop(12) for ligne in res]
+[ligne.pop(11) for ligne in res]
+[ligne.pop(8) for ligne in res]
+[ligne.pop(6) for ligne in res]
+[ligne.pop(5) for ligne in res]
+[ligne.pop(4) for ligne in res]
+[ligne.pop(3) for ligne in res]
+[ligne.pop(2) for ligne in res]
+[ligne.pop(1) for ligne in res]
+
+medailles = []
+for athlete in res:
+    if ("Bronze" in athlete[-1]) or ("Silver" in athlete[-1]) or ("Gold" in athlete[-1]):
+        medailles.append(athlete)
 '''
+annee_ = [-1]
+for athlete in medailles:
+    if athlete[-1] not in
+
+# print(medailles)
+
+medailles_pays
+for noc in country:
+    for cas in medailles:
+        if medailles[cas][1] == noc:
+            medailles_pays.append()
+'''
+
+pays = []
+annees_paricipation = []
+
+moyenne_medaille_pays = []
+for ligne in country:
+    moyenne = somme / len(annees_participation)
+    moyenne_medaille_pays.append([ligne[0], ligne[1], moyenne])
+
+print(pays)
